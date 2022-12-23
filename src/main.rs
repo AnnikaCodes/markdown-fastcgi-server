c6363e466877641a8961b4f6ef58769f1ebb2a79
Annika <annika0uwu@gmail.com>
2022-12-23 23:20:35 +0000

    Things are working

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-//! FastCGI server that takes the FILE parameter to a Markdown file and renders it to HTML.
+//! mdtransform - small tool that converts Markdown to simple HTML
 
-use std::{io::Write, net::TcpListener, path::PathBuf, fs, ffi::OsStr};
-use structopt::{StructOpt, clap::Arg};
-use pulldown_cmark::{HeadingLevel, Event, Tag, CowStr};
+use pulldown_cmark::{CowStr, Event, HeadingLevel, Tag};
+use regex::Regex;
+use std::{ffi::OsStr, fs, path::PathBuf};
+use structopt::StructOpt;
 
-// If you're using this yourself, you'll probably want to change this :)
-// TODO: move this to a template file
+// TODO: support <title> prefix
 static DEFAULT_TEMPLATE: &'static str = r#"
 <!DOCTYPE html>
 <html>
@@ -17,10 +17,6 @@ static DEFAULT_TEMPLATE: &'static str = r#"
                 font-family: sans-serif;
                 font-size: 1.1rem;
             }
-            h1 {
-                text-align: center;
-                font-family: Tahoma, Verdana, Arial, sans-serif;
-            }
         </style>
     </head>
     <body>
@@ -28,6 +24,12 @@ static DEFAULT_TEMPLATE: &'static str = r#"
     </body>
     </html>
 "#;
+
+lazy_static::lazy_static! {
+    static ref HEADING_REGEX: Regex = Regex::new("[^a-z-]*").expect("couldn't compile regex");
+    static ref HTML_EXTENSION: Option<&'static OsStr> = Some(OsStr::new("html"));
+    static ref MARKDOWN_EXTENSION: Option<&'static OsStr> = Some(OsStr::new("md"));
+}
 
 #[derive(Debug, StructOpt)]
 #[structopt(
@@ -42,7 +44,11 @@ struct Arguments {
     )]
     template_path: Option<PathBuf>,
 
-    #[structopt(help = "Markdown files or directories containing .md files", name = "files or directories to process", required = true)]
+    #[structopt(
+        help = "Markdown files or directories containing .md files",
+        name = "files or directories to process",
+        required = true
+    )]
     input_dirs_and_files: Vec<PathBuf>,
 }
 
@@ -54,31 +60,57 @@ fn md_to_html(markdown: String, template: &String) -> String {
     md_parser_options.insert(pulldown_cmark::Options::ENABLE_TASKLISTS);
 
     let mut heading_level: Option<HeadingLevel> = None;
-    let md_parser = pulldown_cmark::Parser::new_ext(&markdown, md_parser_options).filter_map(|event| match event {
-        Event::Start(Tag::Heading(level, _, _)) => {
-            heading_level = Some(level);
-            None
-        }
-        Event::Text(text) => {
-            if let Some(level) = heading_level {
-                let anchor = text
-                    .clone()
-                    .into_string()
-                    .trim()
-                    .to_lowercase()
-                    .replace(" ", "-");
-                let tmp = Event::Html(CowStr::from(format!(
-                    "<{} id=\"{}\">{}",
-                    level, anchor, text
-                )))
-                .into();
-                heading_level = None;
-                return tmp;
+    let md_parser = pulldown_cmark::Parser::new_ext(&markdown, md_parser_options).filter_map(
+        |event| match event {
+            Event::Start(Tag::Heading(level, _, _)) => {
+                heading_level = Some(level);
+                None
             }
-            Some(Event::Text(text))
-        }
-        _ => Some(event),
-    });
+            Event::Text(text) => {
+                if let Some(level) = heading_level {
+                    // Handle title directive
+                    if level == HeadingLevel::H1 {
+                        if let Some(title) = text.strip_prefix("TITLE: ") {
+                            let html = [
+                                "<title>",
+                                title,
+                                "</title>\n",
+                                "<center>\n",
+                                "\t<h1 style='margin-bottom: 0px; font-size: 2.5rem;'>",
+                                title,
+                                "</h1>\n",
+                                "\t<hr />\n",
+                                "</center>",
+                            ]
+                            .join("");
+
+                            heading_level = None;
+
+                            // We don't need a heading for a title, do we?
+                            return Some(Event::Html(CowStr::from(html)));
+                        }
+                    }
+
+                    let anchor = text
+                        .clone()
+                        .into_string()
+                        .trim()
+                        .to_lowercase()
+                        .replace(" ", "-");
+                    let anchor = HEADING_REGEX.replace_all(&anchor, "");
+                    let tmp = Event::Html(CowStr::from(format!(
+                        "<{} id=\"{}\">{}",
+                        level, anchor, text
+                    )))
+                    .into();
+                    heading_level = None;
+                    return tmp;
+                }
+                Some(Event::Text(text))
+            }
+            _ => Some(event),
+        },
+    );
 
     // could be more efficient but eh
     let mut content = String::new();
@@ -90,16 +122,25 @@ fn md_to_html(markdown: String, template: &String) -> String {
 fn process_path(path: PathBuf, template: &String) -> std::io::Result<()> {
     if path.is_dir() {
         for entry in path.read_dir()? {
-            process_path(entry?.path(), template);
+            process_path(entry?.path(), template)?;
         }
     } else if path.is_file() {
-        if path.extension() == Some(OsStr::new("md")) {
-            process_file(path, template);
+        let ext = path.extension();
+        if ext == *MARKDOWN_EXTENSION {
+            process_file(path, template)?;
         } else {
-            eprintln!("Warning: ignoring non-Markdown file '{}'", path.display());
+            if ext != *HTML_EXTENSION {
+                eprintln!(
+                    "Warning: ignoring non-Markdown, non-HTML file '{}'",
+                    path.display()
+                );
+            }
         }
     } else {
-        eprintln!("Warning: ignoring non-file, non-directory '{}'", path.display());
+        eprintln!(
+            "Warning: ignoring non-file, non-directory '{}'",
+            path.display()
+        );
     }
 
     Ok(())
@@ -117,12 +158,15 @@ fn process_file(input_path: PathBuf, template: &String) -> std::io::Result<()> {
 
     output_path.set_extension("html");
     if &output_path.to_string_lossy() == &input_path.to_string_lossy() {
-        eprintln!("Warning: output for '{}' may overwrite the original file — ignoring", input_path.display());
+        eprintln!(
+            "Warning: output for '{}' may overwrite the original file — ignoring",
+            input_path.display()
+        );
         return Ok(());
     }
 
     fs::write(&output_path, md_to_html(markdown_text, template))?;
-    println!("{}", output_path.display());
+    println!("=> {}", output_path.display());
     Ok(())
 }
 
@@ -132,14 +176,21 @@ fn main() -> std::io::Result<()> {
     let template = if let Some(template_path) = args.template_path {
         let template_string = match fs::read_to_string(&template_path) {
             Err(e) => {
-                eprintln!("Error: Couldn't read template file '{}': {}", template_path.display(), e);
+                eprintln!(
+                    "Error: Couldn't read template file '{}': {}",
+                    template_path.display(),
+                    e
+                );
                 std::process::exit(1);
             }
             Ok(str) => str,
         };
 
         if !template_string.contains("$$CONTENT$$") {
-            eprintln!("Error: Template file '{}' does not include '$$CONTENT$$' - exiting.", template_path.display());
+            eprintln!(
+                "Error: Template file '{}' does not include '$$CONTENT$$' - exiting.",
+                template_path.display()
+            );
             std::process::exit(1);
         }
 
@@ -149,11 +200,10 @@ fn main() -> std::io::Result<()> {
     };
 
     for path in args.input_dirs_and_files {
-        process_path(path, &template);
+        process_path(path, &template)?;
     }
 
-
-            // write!(&mut req.stdout(), "{}", HTML_PREFIX).unwrap();
-            // write!(&mut req.stdout(), "{}", HTML_SUFFIX).unwrap();\
+    // write!(&mut req.stdout(), "{}", HTML_PREFIX).unwrap();
+    // write!(&mut req.stdout(), "{}", HTML_SUFFIX).unwrap();\
     Ok(())
 }
---
+++ test/hi.md
@@ -0,0 +1,12 @@
+# TITLE: This is a title directive
+# This is a regular heading
+
+Here's some text, and a [link](https://google.com).
+
+What about a list?
+- thing
+- thing2
+  - thing3
+
+## Here's a subheading
+What about <strong>some</strong> inline HTML? <button onclick="alert('hi');">Click me!</button>
